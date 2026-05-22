540de5f4fcb52e01205c88340a0129c010faa384
Hadi Arrasidin <hadiarrasidin@gmail.com>
2026-05-22 09:22:06 +0700

    fix supabase config use env variable

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://njzcxmszjcicwpxrkszg.supabase.co'; 
-const SUPABASE_ANON_KEY = 'sb_publishable_yo_S7xptB6xR7Sftu2WCsw_xEOtno1f';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
